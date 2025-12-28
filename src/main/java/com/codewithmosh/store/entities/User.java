7850fb759128f71e8627fd7b514a84d5d5d7cfdf
zaki <zakaprog@gmail.com>
2025-12-29 00:53:00 +0100

    using lombok

--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -3,7 +3,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
 @Entity
 @Table(name="users")
 public class User{
@@ -16,28 +19,4 @@ public class User{
     private String email;
     @Column(nullable = false,name = "password")
     private String password;
-    public long getId(){
-        return id;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
 }
