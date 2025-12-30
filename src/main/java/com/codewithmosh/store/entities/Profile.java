c7520a6c0e6d93941cabda6110afc9a80e7a7e97
zaki <zakaprog@gmail.com>
2025-12-30 23:18:44 +0100

    adding some entities

---
+++ src/main/java/com/codewithmosh/store/entities/Profile.java
@@ -0,0 +1,31 @@
+package com.codewithmosh.store.entities
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jdk.jfr.Unsigned;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "profiles")
+public class Profile{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+    @Column(name = "bio")
+    private String bio;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "date_of_birth")
+    private LocalDate Date;
+    @Column(name = "loyalty_points")
+    private int loyaltyPoints ;
+}
--- src/main/java/com/codewithmosh/store/entities/Tag.java
+++ src/main/java/com/codewithmosh/store/entities/Tag.java
@@ -1,4 +1,4 @@
-package com.codewithmosh.store.entities;
+package com.codewithmosh.store.entities
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,19 +7,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-@Setter
-@Getter
+
 @Entity
-@Table(name="users")
-public class User{
+@Getter
+@Setter
+@Table(name = "tags")
+public class Tag{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false,name = "id")
-    private long id;
-    @Column(nullable = false,name = "name")
+    @Column(name = "id",nullable = false)
+    private Long id;
+    @Column(name = "name")
     private String name;
-    @Column(nullable = false,name = "email")
-    private String email;
-    @Column(nullable = false,name = "password")
-    private String password;
 }
